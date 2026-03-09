3cdfe842f0ad9472032238dd7c821ead45b5a885
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2026-03-09 16:57:14 -0300

    finalizando

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,21 +1,25 @@
-import { Component, Input, OnChanges, OnInit, Output, SimpleChanges, EventEmitter } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, Output, SimpleChanges, EventEmitter, OnDestroy } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit, OnChanges {
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
   @Input() item!: Item;
   @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoIdParaDeletar = new EventEmitter();
+
 
 
   faPen = faPen;
   faTrash = faTrash
 
-  constructor() { }
+  constructor(private listaService: ListaDeCompraService) { }
+  
 
   ngOnInit(): void {
 
@@ -27,6 +31,20 @@ export class ItemComponent implements OnInit, OnChanges {
 
   editarItem(){
     this.emitindoItemParaEditar.emit(this.item);
+  }
+
+  alterarStatusItem(){
+    this.item.comprado = !this.item.comprado;
+    this.listaService.atualizarLocalStorage();
+  }
+
+  deletarItem(){
+    console.log("Estão tentando me calar.")
+    this.emitindoIdParaDeletar.emit(this.item.id);
+  }
+
+  ngOnDestroy(){
+    console.log("Conseguiram me calar.")
   }
 
 }
